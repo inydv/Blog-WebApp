c3b9e00472ae339e76c93c43118aeabcc57f26b8
Lokesh Yadav <i_nydv@hotmail.com>
2022-04-06 15:37:51 +0530

    Added multer

--- index.js
+++ index.js
@@ -19,6 +19,22 @@ mongoose
   .then(console.log("connected to mongoDB"))
   .catch((err) => console.log(err));
 
+// MULTER
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, "hello.jpeg");
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
 // LOCAL FILES
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
