b0b686feaffabc533639b9c946d879ba20e746d3
Lokesh Yadav <i_nydv@hotmail.com>
2022-04-06 15:16:47 +0530

    Added query

--- routes/posts.js
+++ routes/posts.js
@@ -56,10 +56,23 @@ router.delete("/:id", async (req, res) => {
 });
 
 // GET POST
-router.get("/:id", async (req, res) => {
+router.get("/", async (req, res) => {
+  const username = req.query.user;
+  const catName = req.query.cat;
   try {
-    const post = await Post.findById(req.params.id);
-    res.status(200).json(post);
+    let posts;
+    if (username) {
+      posts = await Post.find({ username: username });
+    } else if (catName) {
+      posts = await Post.find({
+        categories: {
+          $in: [catName], // include
+        },
+      });
+    } else {
+      posts = await Post.find();
+    }
+    res.status(200).json(posts);
   } catch (err) {
     res.status(401).json(err);
   }
